63fab5ff29b6980802c89b5b7019f01dc4d94edf
Dipesh Kumar Rajak <dipeshme72@gmail.com>
2022-09-18 19:14:10 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum{
+    public static void main(String args[]){
+         int a = 12;
+         int b = 43;
+         int sum = a+b;
+         System.out.println("the sum is "+sum);
+    }
+}
